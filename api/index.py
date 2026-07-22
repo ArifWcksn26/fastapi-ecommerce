90787415600df78770c9e87196e65e255978be49
Arif Wicaksono <arifwicaksono947@gmail.com>
2026-07-23 01:19:02 +0700

    fix vercel entrypoint

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# Memastikan root folder masuk ke sys.path supaya import internal project aman
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
